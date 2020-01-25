9241dfe71e8eb5aadc66fbbb5a795f8fb77f682e
Carlos Trejo <guadarramac74@gmail.com>
2020-01-25 17:00:26 -0600

    finished reload command

--- flight-attendance/cogs/basic.py
+++ flight-attendance/cogs/basic.py
@@ -19,10 +19,19 @@ class Basic(commands.Cog):
     async def reload(self, ctx):
         for filename in os.listdir('flight-attendance/cogs'):
             if filename.endswith('.py'):
-                
-                self.client.reload_extension(f'cogs.{filename[:-3]}')
-        else:
-            await ctx.send('Reload Successful! :+1:')
+                cog_name = filename[:-3]
+
+                if cog_name.title() not in self.client.cogs: # Load the cog instead of reloading it if it wasn't loaded
+                    self.client.load_extension(f'cogs.{cog_name}')
+                else:
+                    self.client.reload_extension(f'cogs.{cog_name}')
+
+        else: await ctx.send('Reload Successful! :+1:')
+
+    @commands.command()
+    async def cogs(self, ctx):
+        await ctx.send(f"{[*self.client.cogs]}")
+
 
 def setup(client):
     client.add_cog(Basic(client))
