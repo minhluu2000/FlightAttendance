ffcbc8c4da035559f16abe4336636cf3c721774e
Carlos Trejo <guadarramac74@gmail.com>
2020-01-25 16:10:59 -0600

    added reload command, renamed example to basic

--- flight-attendance/__main__.py
+++ flight-attendance/__main__.py
@@ -1,4 +1,4 @@
-import os, sys, traceback
+import os
 import discord
 from discord.ext import commands
 
--- flight-attendance/cogs/basic.py
+++ flight-attendance/cogs/basic.py
@@ -1,7 +1,8 @@
 import discord
+import os
 from discord.ext import commands
 
-class Example(commands.Cog):
+class Basic(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -14,5 +15,14 @@ class Example(commands.Cog):
     async def ping(self, ctx):
         await ctx.send('Pong!')
 
+    @commands.command()
+    async def reload(self, ctx):
+        for filename in os.listdir('flight-attendance/cogs'):
+            if filename.endswith('.py'):
+                
+                self.client.reload_extension(f'cogs.{filename[:-3]}')
+        else:
+            await ctx.send('Reload Successful! :+1:')
+
 def setup(client):
-    client.add_cog(Example(client))
+    client.add_cog(Basic(client))
