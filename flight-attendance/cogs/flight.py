f4a5b56a03a19ec7ab29689940dc7d19e3dec86e
Carlos Trejo <guadarramac74@gmail.com>
2020-01-26 00:16:00 -0600

    finished majority of flight cog

--- flight-attendance/cogs/flight.py
+++ flight-attendance/cogs/flight.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 import discord
 from discord.ext import commands
 
@@ -12,8 +13,32 @@ class Flight(commands.Cog):
 
     @commands.command()
     async def flight(self, ctx, *args):
-        depart, arrive = args[1::2]
-        await ctx.send(f"Depart: {depart.title()}\nArrive: {arrive.upper() if len(arrive) == 2 else arrive.title()}")
+        if not args:
+            def check(m): return m.author == ctx.message.author
+
+            flight_info = []
+            
+            dialog = (
+                f"When would you like to fly {ctx.message.author.mention}?",
+                "Sounds great! Where would you like to fly?",
+                "Alrighty! Where would you like to depart from?")
+            
+            
+            for msg in dialog:
+                await ctx.send(msg)
+                response = await self.client.wait_for('message', check=check)
+                flight_info.append(response.content)
+            else:
+                await ctx.send(flight_info)
+            
+        else:
+            if (args[0].lower() != 'from') and (args[2].lower() != 'to'):
+                await ctx.send('Usage: .flight from {location} to {location}')
+            else:
+                depart, arrive = args[1], ' '.join(args[3:])
+                async with ctx.typing():
+                    await sleep(2)
+                    await ctx.send(f"Depart: {depart.title()}\nArrive: {arrive.upper() if len(arrive) == 2 else arrive.title()}")
 
 
 def setup(client):
